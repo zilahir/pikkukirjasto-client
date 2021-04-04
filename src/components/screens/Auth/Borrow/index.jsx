2b79c3cf267f5d3f667974ce1b4bb0ccac8f4dea
Richard Zilahi <richard.zilahi@salesframe.com>
2021-04-04 19:41:14 +0300

    reading qr code

--- src/components/screens/Auth/Borrow/index.jsx
+++ src/components/screens/Auth/Borrow/index.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import QrReader from 'modern-react-qr-reader'
 import Layout from '../../../common/Layout'
 
-const Borrow = () => (
-	<Layout>
-		<div>lofasz</div>
-	</Layout>
-)
+import styles from './Borrow.module.scss'
+
+const Borrow = () => {
+	const [isbn, setIsbn] = useState()
+
+	/**
+	 * @param data
+	 */
+	function handleQrRead(data) {
+		if (data) {
+			console.debug('data', data)
+			setIsbn(data)
+			console.debug('isbn', isbn)
+		}
+	}
+	return (
+		<Layout>
+			<div className={styles.borrowContainer}>
+				<QrReader delay={300} onScan={handleQrRead} />
+				<p>isbn: {isbn}</p>
+			</div>
+		</Layout>
+	)
+}
 
 export default Borrow
